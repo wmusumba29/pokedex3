1e0ead007288c5ad55d0dc652c81c25409a700ba
William Musumba <william@Williams-MacBook-Pro.local>
2017-09-26 22:11:03 +0100

    collection view and cell

---
+++ pokedex3/View/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by William Musumba on 26/09/2017.
+//  Copyright © 2017 William Musumba. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
